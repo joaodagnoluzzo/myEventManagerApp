b10673f4700542d38586a472c0a48ded68123bb7
João Pedro Cappelletto D'Agnoluzzo <joao.dagnoluzzo@gmail.com>
2019-11-15 11:29:33 -0300

    Initial Commit

---
+++ myEventManagerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myEventManagerApp
+//
+//  Created by João Pedro Cappelletto D'Agnoluzzo on 15/11/19.
+//  Copyright © 2019 João Pedro Cappelletto D'Agnoluzzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ myEventManagerAppTests/myEventManagerAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  myEventManagerAppTests.swift
+//  myEventManagerAppTests
+//
+//  Created by João Pedro Cappelletto D'Agnoluzzo on 15/11/19.
+//  Copyright © 2019 João Pedro Cappelletto D'Agnoluzzo. All rights reserved.
+//
+
+import XCTest
+@testable import myEventManagerApp
+
+class myEventManagerAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
