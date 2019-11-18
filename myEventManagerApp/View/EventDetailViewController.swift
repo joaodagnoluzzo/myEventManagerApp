f128fa1dd96c4b46ae921788163a1c586999e775
João Pedro Cappelletto D'Agnoluzzo <joao.dagnoluzzo@gmail.com>
2019-11-17 22:12:50 -0300

    Added tap handling for share button. Yet no action is performed when one of the options is selected.

--- myEventManagerApp/View/EventDetailViewController.swift
+++ myEventManagerApp/View/EventDetailViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class EventDetailViewController: UIViewController {
     
@@ -16,8 +18,12 @@ class EventDetailViewController: UIViewController {
     @IBOutlet weak var eventDateLabel: UILabel!
     @IBOutlet weak var eventPriceLabel: UILabel!
     @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    @IBOutlet weak var checkinButton: UIBarButtonItem!
+    
     
     private let eventViewModel = EventViewModel()
+    private let disposeBag = DisposeBag()
     
     public var event: Event? = nil
     
@@ -35,6 +41,52 @@ class EventDetailViewController: UIViewController {
         self.descriptionTextView.text = event.description
         self.eventImageView.load(url: URL(string: event.image!)!)
         self.eventImageView.contentMode = .scaleToFill
+        
+        setupShareButton()
+    }
+    
+    func setupCheckinButton(){
+        
+        self.checkinButton
+        .rx
+        .tap
+            .subscribe(onNext:{
+                [weak self] in
+                
+                //present modal view for checkin
+                
+            }).disposed(by: disposeBag)
+        
+    }
+    
+    func setupShareButton(){
+        self.shareButton
+        .rx
+        .tap
+            .subscribe(onNext:{
+                [weak self] in
+                
+                let actionSheet = UIAlertController(title: "Compartilhar", message: nil, preferredStyle: .actionSheet)
+                
+                actionSheet.addAction(UIAlertAction(title: "Facebook", style: .default) { (UIAlertAction) in
+                    print("facebook")
+                })
+                
+                actionSheet.addAction(UIAlertAction(title: "Twitter", style: .default) { (UIAlertAction) in
+                    print("twitter")
+                })
+                
+                actionSheet.addAction(UIAlertAction(title: "Instagram", style: .default) { (UIAlertAction) in
+                    print("instagram")
+                })
+                
+                actionSheet.addAction(UIAlertAction(title: "Cancelar", style: .cancel) { (UIAlertAction) in
+                    print("cancel")
+                })
+                
+                self?.present(actionSheet, animated: true, completion: nil)
+                
+            }).disposed(by: disposeBag)
     }
     
 }
